b4de3b7478524604d62ab97bdbcea547feb73852
Johan <lastqubit@gmail.com>
2026-01-27 18:53:31 +0100

    finished faucet

--- contracts/Faucet/Faucet.sol
+++ contracts/Faucet/Faucet.sol
@@ -6,20 +6,21 @@ import {Endpoints} from "./Endpoints.sol";
 import {resolveAmount} from "../Lib/Utils.sol";
 
 contract Faucet is Node, Endpoints {
-    uint constant balance = 500; // **18 ??
+    uint constant BALANCE = 1000 * (10 ** 18);
+    uint immutable eid = initiateId;
 
-    constructor(address rush, address discovery) Node(rush, discovery, "faucet") {}
+    constructor(address cmdr, address discovery) Node(cmdr, discovery, "faucet") {}
 
     function debitFrom(uint account, uint id, uint min, uint max) internal override returns (uint) {
-        uint amount = resolveAmount(balance, min, max);
-        uint total = balance - amount;
-        emit Balance(account, initiateId, id, total, amount);
+        uint amount = resolveAmount(BALANCE, min, max);
+        uint balance = BALANCE - amount;
+        emit Balance(account, eid, id, balance, amount);
         return amount;
     }
 
     function creditTo(uint account, uint id, uint amount) internal override returns (uint) {
-        uint total = balance + amount;
-        emit Balance(account, initiateId, id, total, amount);
+        uint balance = BALANCE + amount;
+        emit Balance(account, eid, id, balance, amount);
         return amount;
     }
 }
