99570f0514faf2419731b1007061b78997f4e6b5
Johan <lastqubit@gmail.com>
2026-03-16 16:09:05 +0100

    Rename balance mapping asset key

--- contracts/core/Balances.sol
+++ contracts/core/Balances.sol
@@ -4,5 +4,6 @@ pragma solidity ^0.8.33;
 import {BalanceEvent} from "../events/Balance.sol";
 
 abstract contract Balances is BalanceEvent {
-    mapping(bytes32 account => mapping(bytes32 asset => uint amount)) internal balances;
+    mapping(bytes32 account => mapping(bytes32 assetRef => uint amount))
+        internal balances;
 }
